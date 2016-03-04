c20746bcd3be4cf84a60d5d681d6470c67b6e8a5
DAndrewA <andrew.martin@bourne-grammar.lincs.sch.uk>
2016-03-04 23:00:16 +0000

    menu works

---
+++ beaconGame/app/src/main/java/andrewmmattb/beacongame/GameActivity.java
@@ -0,0 +1,30 @@
+package andrewmmattb.beacongame;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class GameActivity extends Activity {
+
+    String username;
+    TextView usernameTextView;
+    TextView scoreTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        usernameTextView = (TextView)findViewById(R.id.textViewGameUsername);
+        scoreTextView = (TextView)findViewById(R.id.textViewGameScore);
+
+        Intent intent = getIntent();
+        username = intent.getStringExtra("username");
+        usernameTextView.setText(username);
+    }
+}
