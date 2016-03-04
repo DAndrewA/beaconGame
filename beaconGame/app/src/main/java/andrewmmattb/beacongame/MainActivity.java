7d6de11e0b3c182fa922f4e67181e017fcf00d00
DAndrewA <andrew.martin@bourne-grammar.lincs.sch.uk>
2016-03-04 23:44:17 +0000

    menu finished for now

--- beaconGame/app/src/main/java/andrewmmattb/beacongame/MainActivity.java
+++ beaconGame/app/src/main/java/andrewmmattb/beacongame/MainActivity.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    // creating the objects
     Button buttonGo;
     EditText usernameInput;
 
@@ -19,9 +23,11 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // assigning the view objects values
         buttonGo = (Button)findViewById(R.id.buttonGo);
         usernameInput = (EditText)findViewById(R.id.usernameInput);
 
+        // sets the button click function
         buttonGo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -30,10 +36,17 @@ public class MainActivity extends Activity {
         });
     }
 
+    // starts the game activity with the username and background colour passed through
     void joinGame(){
         String username = String.valueOf(usernameInput.getText());
-        Intent intent = new Intent(this, GameActivity.class);
-        intent.putExtra("username",username);
-        startActivityForResult(intent,1);
+
+        if(username.length() <= 0){
+            Toast.makeText(MainActivity.this, "Please enter a name", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Intent intent = new Intent(this, GameActivity.class);
+            intent.putExtra("username",username);
+            startActivityForResult(intent,1);
+        }
     }
 }
